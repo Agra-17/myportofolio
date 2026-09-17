b982444fe4b63346739062e19cb7d567ec096c46
Muh. Agra Putra Davyza Chaniago <muh.agra@ui.ac.id>
2026-09-17 22:32:36 +0700

    feat: Update main/urls.py (Adding new path create, delete, and get experience

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import achievement_list, show_main, show_experience, create_achievement, get_achievements_json, delete_achievement
+from main.views import achievement_list, show_main, show_experience, create_achievement, get_achievements_json, delete_achievement, create_experience, get_experience_json, delete_experience
 
 app_name = "main"
 
@@ -10,5 +10,8 @@ urlpatterns = [
     path("achievements/", achievement_list, name="show_achievements"),
     path("achievements/add/", create_achievement, name="create_achievements"),
     path("api/achievements/", get_achievements_json, name="get_achievements_json"),
-    path("achievements/<uuid:achievement_id>/delete/", delete_achievement, name="delete_achievement")
+    path("achievements/<uuid:achievement_id>/delete/", delete_achievement, name="delete_achievement"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path("api/experience/", get_experience_json, name="get_experience_json"),
+    path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience")
 ]
